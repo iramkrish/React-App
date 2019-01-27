cca081f8a2e2d9bc8341d0b3be6df9dc3c8b6706
ramkrishnan <iramkrish95@gmail.com>
2019-01-27 10:31:33 +0530

    Updates

---
+++ precache-manifest.158aab8de388d1decd2a3e62b2502daa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abfeebd436016ce1da09",
+    "url": "/React-App/static/js/runtime~main.abfeebd4.js"
+  },
+  {
+    "revision": "51b0cf68c26d50fa0df3",
+    "url": "/React-App/static/js/main.51b0cf68.chunk.js"
+  },
+  {
+    "revision": "317ea1ffc26043cc94c8",
+    "url": "/React-App/static/js/1.317ea1ff.chunk.js"
+  },
+  {
+    "revision": "51b0cf68c26d50fa0df3",
+    "url": "/React-App/static/css/main.4701be47.chunk.css"
+  },
+  {
+    "revision": "94416228d29c89cf78c43a80a8fb3509",
+    "url": "/React-App/index.html"
+  }
+];
